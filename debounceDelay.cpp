9b90c329a51db497e4bce5005b503a5595f68c25
Kiruba Pradeep <kiruba777@gmail.com>
2026-06-27 07:45:10 +0530

    Arduino - Debounce push button and serial o/p

---
+++ debounceDelay.cpp
@@ -0,0 +1,39 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int ledState = HIGH;
+int buttonState;
+int lastButtonState = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  Serial.println(ledState);
+  digitalWrite(ledPin, ledState);
+
+  lastButtonState = reading;
+}
